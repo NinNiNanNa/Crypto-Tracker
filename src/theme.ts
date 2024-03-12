92aaa621c4c85813ba01cf81bc6e6667cad972b4
NinNiNanNa <kimsojin3011@gmail.com>
2024-03-12 20:20:25 +0900

    Initial commit

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "",
+  textColor: "",
+  accentColor: "",
+  gradientColor: "",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "",
+  textColor: "",
+  accentColor: "",
+  gradientColor: "",
+};
